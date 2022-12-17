543b4c1fc891875c7a5cf658026782192da08ff4
Twinkle9807 <twinklepatel55136@gmail.com>
2022-12-17 18:48:36 +0530

    upload my c program

---
+++ example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter value of a:";
+    cin>>a;
+    cout<<"Enter value of b:";
+    cin>>b;
+    int sum= a+b;
+    cout<<"The result is: "<<sum;
+    
+    return 0;
+}
